f8d008eaceeb9ff8300faa591aeaf82159c9ca42
ErissonAndrade <erisson5000@hotmail.com>
2023-11-08 23:41:41 -0300

    Added getters and setters in API model class

--- src/main/java/com/personal/PersonalPortfolio/domain/model/API.java
+++ src/main/java/com/personal/PersonalPortfolio/domain/model/API.java
@@ -16,5 +16,53 @@ public class API {
 
     private Portfolio portfolio;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Home getHome() {
+        return home;
+    }
+
+    public void setHome(Home home) {
+        this.home = home;
+    }
+
+    public About getAbout() {
+        return about;
+    }
+
+    public void setAbout(About about) {
+        this.about = about;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
     private Contact contact;
 }
